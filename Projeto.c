0cf2ba85e29cea0d9636f5c79c219371225292a3
JohnnyMinho <jpsousamoura2000@gmail.com>
2023-10-19 18:47:43 +0100

    Tagala

--- Projeto.c
+++ Projeto.c
@@ -24,6 +24,7 @@ unsigned char* Gen_Chave_Mestra(int tamanho_chave){
 }
 
 unsigned char* rotate(char* M, int rotation_number){
+    
     size_t size= strlen(M);
     char* temp_Return = (char*)malloc(size+1);
     memmove(temp_Return,M,size);
@@ -48,9 +49,9 @@ unsigned char* rotate(char* M, int rotation_number){
     return temp_Return;
 }
 
-unsigned char xor(unsigned char Za,unsigned char Zb,unsigned char Zc,unsigned char Zd){
-    printf("Za:%c,Zb:%c,Zc:%c,Zd:%c",Za,Zb,Zc,Zd);
-    unsigned char value_to_return = Za^Zb^Zc^Zd; 
+unsigned char xor(char Za,char Zb,unsigned char Zc,unsigned char Zd){
+    //printf("Za:%c,Zb:%c,Zc:%c,Zd:%c",Za,Zb,Zc,Zd);
+    unsigned char value_to_return = (Za^ Zb ^ Zc ^ Zd); 
     return value_to_return;
 }
 
@@ -58,9 +59,46 @@ unsigned char random_char(char seed,int inc, int max){
     //Assumi que este era o caso uso da seed visto que não estava a ver outra maneira de isto ser feito
     //E na net este foi o exemplo mais frequente que encontrei
     srand(seed);
-    unsigned char Pos_random=(char)('0' + rand() % 255);
+    unsigned char Pos_random=(char)((char) inc + rand() % max);
+    printf("Seed usada:%c",seed);
+    //printf("Valor random obtido: %c\n",Pos_random);
     //printf("Pos_random gerado = %d\n",Pos_random);
     return Pos_random;
+}
+
+unsigned random_charZ(unsigned char seed,int inc, int max){
+    //Assumi que este era o caso uso da seed visto que não estava a ver outra maneira de isto ser feito
+    //E na net este foi o exemplo mais frequente que encontrei
+    srand(seed);
+    unsigned char Pos_random=(char)((char) inc + rand() % max);
+    printf("Seed usada:%c",seed);
+    //printf("Valor random obtido: %c\n",Pos_random);
+    //printf("Pos_random gerado = %d\n",Pos_random);
+    return Pos_random;
+}
+
+unsigned char* rotateZ(unsigned char* M, int rotation_number) {
+    printf("Tagala");
+    size_t size = sizeof(M) / sizeof(M[0]);
+    printf("Tagala");
+    unsigned char* temp_Return = (unsigned char*)malloc(size + 1);
+    printf("Tagala");
+    memmove(temp_Return, M, size);
+    printf("Tagala");
+    unsigned char temp_array[size];
+    printf("Tagala");
+    temp_array[size] = '\0';
+    // Copy the last n elements of the original array to the temporary array.
+    memmove(temp_array, temp_Return + size - rotation_number, rotation_number * sizeof(char));
+    printf("Tagala");
+    // Copy the remaining elements of the original array to the temporary array.
+    memmove(temp_array + rotation_number, temp_Return, size - rotation_number * sizeof(char));
+    printf("Tagala");
+    // Copy the rotated array from the temporary array back to the original array.
+    memmove(temp_Return, temp_array, size);
+    printf("Tagala");
+
+    return temp_Return;
 }
 
 int main(int argc, char *argv[]) {
@@ -103,10 +141,12 @@ int main(int argc, char *argv[]) {
     }*/
     memcpy(Chave_M1,Chave_Mestra,10);
     memcpy(Chave_M2,Chave_Mestra+10,10);
+    /*
     printf("K:%d\n",K);
     printf("Chave_Mestra:%s\n",Chave_Mestra);
     printf("teste:%s\n",Chave_M1);
     printf("teste:%s\n",Chave_M2);
+    */
     //Preenchimento das Matrizes Za & Zb
     int contador_row = 0;
     int contador_collumn = 0;
@@ -118,10 +158,6 @@ int main(int argc, char *argv[]) {
         temp_m[K_T] = '\0';
         memmove(temp_m,Chave_M1,sizeof(temp_m));
         char *rotated_array = rotate(temp_m,contador_row);
-        //printf("Testes alt ChaveM: %s\n", Chave_M1);
-        //printf("Pre_error_Hello\n");
-        //printf("Teste out_of_bounds:%c\n", Chave_M1[15]);
-        //printf("Hello\n");
         for(int contador_collumn=0;contador_collumn < K;contador_collumn++){
             if(contador_row == 0){
                 Za[contador_row][contador_collumn] = Chave_M1[contador_collumn];
@@ -129,7 +165,7 @@ int main(int argc, char *argv[]) {
                 Za[contador_row][contador_collumn] = rotated_array[contador_collumn];
             }
         }
-        //printf("%s\n",Za[contador_row]);
+        printf("%s\n",Za[contador_row]);
     }
     
     //Zb -> Filled per Collumn
@@ -138,7 +174,6 @@ int main(int argc, char *argv[]) {
         temp_m2[K_T] = '\0';
         memmove(temp_m2,Chave_M2,sizeof(temp_m2));
         char *rotated_array2 = rotate(temp_m2,contador_collumn);
-        //printf("Collumn Rotation:%s\n",rotated_array2);
         for(int contador_row=0;contador_row < K;contador_row++){
             if(contador_collumn == 0){
                 Zb[contador_row][contador_collumn] = Chave_M2[contador_row];
@@ -146,7 +181,6 @@ int main(int argc, char *argv[]) {
                 Zb[contador_row][contador_collumn] = rotated_array2[contador_row];
             }
         }
-        //printf("%s\n",Zb[contador_collumn]);
     }
 
     //Zc & Zd -> Filled by random char generated trough the random() function, this random char is calculated from a random number with the original Za or Zb character
@@ -166,21 +200,46 @@ int main(int argc, char *argv[]) {
     }
 
     //Z matrix -> Filled by xored elements from all the previous Matrixes. 
+    //Procurar como gerar ou perguntar ao professor se devemos tentar usar true random numbers em vez de PRNG do C. Podemos usar aplicações nativas do Unix ou Windows
+    //Mas não estou a ver como podemos usar uma seed dessa maneira visto que até agora não encontrei aplicações dessas funções com uma seed
     for(int contador_row=0;contador_row < K;contador_row++){
         for(int contador_collumn=0;contador_collumn < K;contador_collumn++){
-            printf("Iteração: %d",contador_row);
-            unsigned char temp = xor(*Za[contador_row,contador_collumn],
-            *Zb[contador_row,contador_collumn],
-            *Zc[contador_row,contador_collumn],
-            *Zd[contador_row,contador_collumn]);
+            unsigned char temp = xor(Za[contador_row][contador_collumn],
+            Zb[contador_row][contador_collumn],
+            Zc[contador_row][contador_collumn],
+            Zd[contador_row][contador_collumn]);
             Z[contador_row][contador_collumn] = temp;
             printf("Z gerado: %c\n",Z[contador_row][contador_collumn]);
         }
-        //printf("Iterações:%d\n",contador_row);
-        //printf("%s\n",Zc[contador_row]);
-        //printf("Tamanho_array: %d",sizeof(Zc[contador_row]));
     }
 
+    // Process and update Z matrix
+    // Rotate each row a random number
+    for (int contador_row = 0; contador_row < K_T; contador_row++) {
+        int rotation_number = random_char(Z[contador_row][0], 0, K_T - 1);
+        printf("rotaiton_number: %d\n", rotation_number);
+        unsigned char* temp_row = rotateZ(Z[contador_row], rotation_number);
+        printf("Tagala2");
+        //  printf("here\n");
+        for (int contador_collumn = 0; contador_collumn < K_T; contador_collumn++) {
+            Z[contador_row][contador_collumn] = temp_row[contador_collumn];
+        }
+        printf("Tagala2");
+    }
+
+    for (int contador_column = 0; contador_column < K_T; contador_column++) {
+        int rotation_number = random_char(Z[0][contador_column], 0, K_T - 1);
+        unsigned char* column = malloc(K_T * sizeof(char));
+        for (int i = 0; i < K_T; i++) {
+            column[i] = Z[0][i];
+        }
+
+        unsigned char* temp_row = rotateZ(column, rotation_number);
+        //   printf("here\n");
+        for (int contador_row = 0; contador_row < K_T; contador_row++) {
+            Z[contador_row][contador_column] = temp_row[contador_row];
+        }
+    }
 
     return 0;
 }
