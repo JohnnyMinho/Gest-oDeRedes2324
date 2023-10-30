95cffb679da5ad0245cd8d06770ac7717eaba4e4
JohnnyMinho <jpsousamoura2000@gmail.com>
2023-10-30 15:13:46 +0000

    KeyManager adicionado mas não concluido

--- Projeto.c
+++ Projeto.c
@@ -6,14 +6,45 @@
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <arpa/inet.h> //Não sei se podemos usar este
+#include <sys/socket.h> 
 #include <fcntl.h>
+#include <pthread.h>
 
 #define K 10 //Têm de ser retirado do ficheiro de config depois
 //#define Min 0 //Minimo para as funções random, têm de ser retirado do ficheiro em versões posteriores
 //#define Max 255 //Máximo para as funções random, o nosso objetivo é obter valores ASCII válidos, dai 255 como máximo, mesma situação do Min
 //Têmos de criar uma struct para a MIB
 
+typedef int IntegerOid; //Para existir a aproximação máxima à escrita de uma MIB definimos estas variáveis.
+typedef char *OctetStringOid;
 
+
+/*Não mexer mais nesta parte até termos o protrocolo de tratamento de requests definindo
+typedef struct {
+  IntegerOid oid;
+  char *name;
+  int syntax;
+  int maxAccess;
+  int status;
+  char *description;
+} ObjectType;
+
+// Struct para a Tabela
+typedef struct {
+  IntegerOid keyId;
+  OctetStringOid keyValue;
+  OctetStringOid keyRequester;
+  IntegerOid keyExpirationDate;
+  IntegerOid keyExpirationTime;
+  IntegerOid keyVisibility;
+} DataTableGeneratedKeysEntry;
+*/
+
+typedef struct{
+    int K_T; //Basicamente K
+    int T; //Tempo entre atualizações
+} dados_KeyAgent;
 
 unsigned char* Gen_Chave_Mestra(int tamanho_chave){
     int ltime = time(NULL);
@@ -26,7 +57,6 @@ unsigned char* Gen_Chave_Mestra(int tamanho_chave){
         chave_formada[i]=(char)('0' + rand() % 10);
         //printf("Chave:%c\n",chave_formada[i]);
     }
-    
     return chave_formada;
 }
 
@@ -100,92 +130,24 @@ unsigned char* rotateZ(unsigned char* M, int rotation_number) {
     return temp_Return;
 }
 
-int main(int argc, char *argv[]) {
-    //Tempo retirado com a inicilização do programa (Time_Since_Initiliazed)
-    struct timeval tv;
-    gettimeofday(&tv,NULL);
-    int Time_Initilized_sec = tv.tv_sec; // seconds
-    long int Time_Initilized_microsec = tv.tv_usec; // microseconds
+//Definição das funções para a threads.
 
-    //Ficheiro de Configuração, Campos:
-    //-> Porta UDP usada, Adresso servidor, Adresso Cliente , Tamanho de K, Min e Max caracteres possíveis para a função random,
-    //-> De quantos em quantos segundos as tabelas são atualizadas
-    int config_fd = 0;
-    int Port_Server = 0;
-    char Ip_Address_temp[15];
-    memset(Ip_Address_temp,'\0',sizeof(Ip_Address_temp));
-    size_t K_T = K; //Ao que parece se for realizada qualquer operação sobre um valor do #define o mesmo fica alterado permanentemente
+void KeyAgent(void *arg){
+
+    dados_KeyAgent *dados = (dados_KeyAgent *)arg;
+    int K_T = dados->K_T;
+    int T = dados->T;
     int Min = 0;
     int Max = 255;
-    int T = 0; //Intervalo de tempo entre atualizações
-    int Start_up_time = 0; //Tempo desde o início do programa
 
-    if(argc<2){ //Abertura de ficheiro default
-        config_fd = open("config_SNMPKeys.txt", O_RDONLY);
-    }else{ //Abertura de um ficheiro custom, se quisermos usar isto têmos de defenir um plano que deve ser seguido para que a leitura do ficheiro seja sempre respeitada
-        config_fd = open(argv[1],O_RDONLY);
-    }
-    
-    if(config_fd == -1){
-        perror("Erro relacionado com o ficheiro de conversão");
-        return -1;
-    }
-    
-    char buffer[1024];
-    int n = 0;
-
-
-    while((n=read(config_fd,buffer,sizeof(buffer)))>0){ //Continua a haver coisas a ler no ficheiro de config
-        int contador = 0;
-        char *temp_ptr;
-        char *token = strtok(buffer,",");
-        while(token != NULL){
-        switch (contador)
-        {
-        case 0:
-            Port_Server = atoi(token);
-            break;
-        case 1:
-            memcpy(Ip_Address_temp,token,strlen(token));
-            break;
-        case 2:
-            K_T = atoi(token);
-            break;
-        case 3:
-            Min = atoi(token);
-            break;
-        case 4:
-            Max = atoi(token);
-            break;
-        case 5:
-            T = atoi(token);  
-            break;
-        default:
-            break;
-        }
-        token = strtok(NULL,",");
-        contador++;
-        }
-    }
-    
-    //Tratamento extra ligado ao endereço Ip.
-    char Ip_Address[strlen(Ip_Address_temp)+1];
-    memmove(Ip_Address,Ip_Address_temp,sizeof(Ip_Address));
-    Ip_Address[strlen(Ip_Address_temp)+1] == '\0';  
-
-    close(config_fd);
-
-    //Inicilização do Servidor
-    
-  //Temos de fazer a leitura do ficheiro para tirar as configurações, têmos também de decidir como fica o formato
-    
+    //Geração das matrizes e inicialização do keyagent    
     unsigned char Za[K_T][K_T];
     unsigned char Zb[K_T][K_T];
     unsigned char Zc[K_T][K_T];
     unsigned char Zd[K_T][K_T];
     unsigned char Z[K_T][K_T];
     
-   for(int contador_row=0;contador_row != K_T;contador_row++){
+    for(int contador_row=0;contador_row != K_T;contador_row++){
         for(int contador_collumn=0;contador_collumn != K_T;contador_collumn++){
             //printf("Teste_NUm:%d",contador_collumn);
             Za[contador_row][contador_collumn] = '\0';
@@ -202,22 +164,10 @@ int main(int argc, char *argv[]) {
     unsigned char Chave_M2[K_T];
     Chave_M1[K_T] = '\0';
     Chave_M2[K_T] = '\0';
-    /*for(int N_chaves = 0;N_chaves<2;N_chaves++){
-        printf("N_chaves:%d\n",N_chaves);
-        if(N_chaves == 0){
-            memmove(Chave_MX[N_chaves],Chave_Mestra,K*sizeof(char)); //Ficamos com os 2 pares de chaves necessários
-        }else{
-            memmove(Chave_MX[N_chaves],Chave_Mestra+((N_chaves)*K),K*sizeof(char)); //Ficamos com os 2 pares de chaves necessários
-        }
-    }*/
+
     memcpy(Chave_M1,Chave_Mestra,10);
     memcpy(Chave_M2,Chave_Mestra+10,10);
-    /*
-    printf("K:%d\n",K);
-    printf("Chave_Mestra:%s\n",Chave_Mestra);
-    printf("teste:%s\n",Chave_M1);
-    printf("teste:%s\n",Chave_M2);
-    */
+
     //Preenchimento das Matrizes Za & Zb
     int contador_row = 0;
     int contador_collumn = 0;
@@ -259,16 +209,9 @@ int main(int argc, char *argv[]) {
 
     for(int contador_row=0;contador_row < K;contador_row++){
         for(int contador_collumn=0;contador_collumn < K;contador_collumn++){
-            //printf("Chamadas Random:%d\n",contador_collumn);
-            
             Zc[contador_row][contador_collumn] = random_char(Za[contador_row][contador_collumn],Min,Max); //Estamos a ficar com chars a mais no array
             Zd[contador_row][contador_collumn] = random_char(Zb[contador_row][contador_collumn],Min,Max);
         }
-        /*printf("Iterações:%d\n",contador_row);
-        printf("%s\n",Zc[contador_row]);
-        printf("Tamanho_array: %d\n",sizeof(Zc[contador_row]));
-        printf("D:%s\n",Zd[contador_row]);
-        printf("Tamanho_arrayD: %d",sizeof(Zd[contador_row]));*/
     }
 
     //Z matrix -> Filled by xored elements from all the previous Matrixes. 
@@ -285,31 +228,135 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // Process and update Z matrix
-    // Rotate each row a random number
-    for (int contador_row = 0; contador_row < K_T; contador_row++) {
-        int rotation_number = random_char(Z[contador_row][0], 0, K_T - 1);
-        printf("rotaiton_number: %d\n", rotation_number);
-        unsigned char* temp_row = rotateZ(Z[contador_row], rotation_number);
-        //  printf("here\n");
-        for (int contador_collumn = 0; contador_collumn < K_T; contador_collumn++) {
-            Z[contador_row][contador_collumn] = temp_row[contador_collumn];
+    //Obtenção do tempo
+    struct timeval tv;
+    int Time_Initilized_sec = tv.tv_sec;
+    int N_atualizados = 0; //Número de atualizações da tabela
+    // -----------------
+    while(1){
+        if(tv.tv_sec-Time_Initilized_sec>30){
+            // Process and update Z matrix
+            // Rotate each row a random number
+            for (int contador_row = 0; contador_row < K_T; contador_row++) {
+                int rotation_number = random_char(Z[contador_row][0], 0, K_T - 1);
+                printf("rotaiton_number: %d\n", rotation_number);
+                unsigned char* temp_row = rotateZ(Z[contador_row], rotation_number);
+                for (int contador_collumn = 0; contador_collumn < K_T; contador_collumn++) {
+                    Z[contador_row][contador_collumn] = temp_row[contador_collumn];
+                }
+            }
+
+            for (int contador_column = 0; contador_column < K_T; contador_column++) {
+                int rotation_number = random_char(Z[0][contador_column], 0, K_T - 1);
+                unsigned char* column = malloc(K_T * sizeof(char));
+                for (int i = 0; i < K_T; i++) {
+                    column[i] = Z[0][i];
+                }
+
+                unsigned char* temp_row = rotateZ(column, rotation_number);
+                //   printf("here\n");
+                for (int contador_row = 0; contador_row < K_T; contador_row++) {
+                    Z[contador_row][contador_column] = temp_row[contador_row];
+                }
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    //Identificadores para cada Thread
+    pthread_t KeyAgent_ID;
+    pthread_t CommManager_ID;
+    pthread_t ProtocolMod_ID;
+    //--------------------------------
+    
+    //Tempo retirado com a inicilização do programa (Time_Since_Initiliaze)
+    struct timeval tv;
+    gettimeofday(&tv,NULL);
+    int Time_Initilized_sec = tv.tv_sec; // seconds
+    long int Time_Initilized_microsec = tv.tv_usec; // microseconds
+    //---------------------------------------------------------------------
+
+    //Ficheiro de Configuração, Campos:
+    //-> Porta UDP usada, Adresso servidor, Adresso Cliente , Tamanho de K, Min e Max caracteres possíveis para a função random,
+    //-> De quantos em quantos segundos as tabelas são atualizadas
+    int config_fd = 0;
+    int Port_Server = 0;
+    char Ip_Address_temp[15];
+    memset(Ip_Address_temp,'\0',sizeof(Ip_Address_temp));
+    size_t K_T = K; //Ao que parece se for realizada qualquer operação sobre um valor do #define o mesmo fica alterado permanentemente
+    int Min = 0;
+    int Max = 255;
+    int T = 0; //Intervalo de tempo entre atualizações
+    int Start_up_time = 0; //Tempo desde o início do programa
+
+    if(argc<2){ //Abertura de ficheiro default
+        config_fd = open("config_SNMPKeys.txt", O_RDONLY);
+    }else{ //Abertura de um ficheiro custom, se quisermos usar isto têmos de defenir um plano que deve ser seguido para que a leitura do ficheiro seja sempre respeitada
+        config_fd = open(argv[1],O_RDONLY);
+    }
+    
+    if(config_fd == -1){
+        perror("Erro relacionado com o ficheiro de conversão");
+        return -1;
+    }
+    
+    char buffer[1024];
+    int n = 0;
+
+    while((n=read(config_fd,buffer,sizeof(buffer)))>0){ //Continua a haver coisas a ler no ficheiro de config
+        int contador = 0;
+        char *temp_ptr;
+        char *token = strtok(buffer,",");
+        while(token != NULL){
+        switch (contador)
+        {
+        case 0:
+            Port_Server = atoi(token);
+            break;
+        case 1:
+            memcpy(Ip_Address_temp,token,strlen(token));
+            break;
+        case 2:
+            K_T = atoi(token);
+            break;
+        case 3:
+            Min = atoi(token);
+            break;
+        case 4:
+            Max = atoi(token);
+            break;
+        case 5:
+            T = atoi(token);  
+            break;
+        default:
+            break;
+        }
+        token = strtok(NULL,",");
+        contador++;
         }
     }
 
-    for (int contador_column = 0; contador_column < K_T; contador_column++) {
-        int rotation_number = random_char(Z[0][contador_column], 0, K_T - 1);
-        unsigned char* column = malloc(K_T * sizeof(char));
-        for (int i = 0; i < K_T; i++) {
-            column[i] = Z[0][i];
-        }
+    //Tratamento extra ligado ao endereço Ip.
+    char Ip_Address[strlen(Ip_Address_temp)+1];
+    memmove(Ip_Address,Ip_Address_temp,sizeof(Ip_Address));
+    Ip_Address[strlen(Ip_Address_temp)+1] == '\0';
 
-        unsigned char* temp_row = rotateZ(column, rotation_number);
-        //   printf("here\n");
-        for (int contador_row = 0; contador_row < K_T; contador_row++) {
-            Z[contador_row][contador_column] = temp_row[contador_row];
-        }
-    }
+    close(config_fd);
+    //---------------------------------------------------------------------
+
+    //Inicilização do Servidor
+
+    int Udp_Server_Socket_fd = (AF_INET, SOCK_DGRAM, 0); //Descriptor da socket UDP, AF_INET especifica que vai trabalhar sobre IpV4
+    //-------------------------
+
+    //Inicialização keyAgent e Struct para passagem de argumentos
+    dados_KeyAgent args_keyagent;
+    args_keyagent.K_T = K_T;
+    args_keyagent.T = T;
+
+    pthread_create(KeyAgent_ID,NULL,KeyAgent,&args_keyagent);
 
     return 0;
 }
