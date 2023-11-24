8283cdfc7dd810400f3e70b18fa98fd15cc96825
João Moura <79586002+JohnnyMinho@users.noreply.github.com>
2023-11-24 13:20:39 +0000

    Add files via upload

--- Projeto.c
+++ Projeto.c
@@ -6,8 +6,9 @@
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <unistd.h>
-#include <arpa/inet.h> //Não sei se podemos usar este
+#include <arpa/inet.h> 
 #include <sys/socket.h> 
+#include <netdb.h>
 #include <fcntl.h>
 #include <pthread.h>
 
@@ -46,8 +47,8 @@ typedef struct{
 } dados_KeyAgent;
 
 typedef struct{
-    unsigned long IP_add;
-    unsigned short port;
+    char* IP_add;
+    int port;
 } dados_CommAgent;
 
 
@@ -118,22 +119,22 @@ unsigned char* rotateZ(unsigned char* M, int rotation_number) {
 //No server usamos a solução sugerida pelo professor que é o uso de um ficheiro com o número de pedidos total
 
 void *CommAgent(void *arg){
-
+    printf("CommAgent Started\n");
     int Udp_Server_Socket_fd = (AF_INET, SOCK_DGRAM, 0); //Descriptor da socket UDP, AF_INET especifica que vai trabalhar sobre IpV4
     dados_CommAgent *dados = (dados_CommAgent *)arg;
     char server_message[2000], client_message[2000];
     struct sockaddr_in server; //Esta socket é mais por questões de facilitar a vida ao usar o inet
     char buffer_numpedidos[11];
     buffer_numpedidos[11] = '\0';
-
-    int fd_rqfile = open("Num_Requests.txt",O_RDWR|O_CREAT|O_TRUNC,0666);
+    int fd_rqfile = open("Num_Requests.txt",O_RDWR);
     read(fd_rqfile,buffer_numpedidos,sizeof(buffer_numpedidos)); //Podiamos optar por uma solução mais bonita a partir do fstat
 
     int  num_pedidos = atoi(buffer_numpedidos); 
-
+    printf("Ip: %s,", dados->IP_add);
+    printf("%lc\n",sizeof(dados->IP_add));
     server.sin_family = AF_INET;
-    server.sin_addr.s_addr = dados->IP_add;
-    server.sin_port = dados->port;
+    server.sin_addr.s_addr = inet_addr(dados->IP_add);
+    server.sin_port = htons(dados->port);
 
     if(Udp_Server_Socket_fd < 0){
         printf("UDP socket creation wasn't possible, shutting down!");
@@ -141,14 +142,13 @@ void *CommAgent(void *arg){
     } //Erro na Criação
 
     //Basicamente dizer que a socket passa a ter estas propriedades
-    if(bind(Udp_Server_Socket_fd, (struct sockaddr*)&server, sizeof(server)) < 0){
-        printf("Couldn't bind to the port\n");
-        exit(-2);
+    while(bind(Udp_Server_Socket_fd, (struct sockaddr*)&server, sizeof(server)) < 0){
+        //É PRECISO FAZER UM TIPO DE TIMEOUT AQUI SENÃO SE REALMENTE ISTO ESTIVER A DAR MAL FICAMOS EM UM LOOP INFINITO
     }
 
     //Verificamos se a socket ficou atribuída
-    socklen_t len_nome_server = sizeof(server);
-    
+    int len_nome_server = sizeof(server);
+    socklen_t length_server = len_nome_server;
     if(getsockname(Udp_Server_Socket_fd,&server,len_nome_server)){
         printf("Port failed to be trully attributed to the specified parameters");
         exit(-3);
@@ -157,7 +157,7 @@ void *CommAgent(void *arg){
 }
 
 void *KeyAgent(void *arg){
-
+    printf("KeyAgent Started\n");
     dados_KeyAgent *dados = (dados_KeyAgent *)arg;
     int K_T = dados->K_T;
     int T = dados->T;
@@ -374,8 +374,8 @@ int main(int argc, char *argv[]) {
     //---------------------------------------------------------------------
 
     //Inicilização do Servidor & Definição dos argumentos
-    args_commagent.port = htons(Port_Server);
-    args_commagent.IP_add = inet_addr(Ip_Address);
+    args_commagent.port = Port_Server;
+    args_commagent.IP_add = Ip_Address;
     
     //-------------------------
 
@@ -386,11 +386,6 @@ int main(int argc, char *argv[]) {
 
     char buffer_numpedidos[11];
     buffer_numpedidos[11] = '\0';
-
-    int fd_rqfile = open("Num_Requests.txt",O_RDWR|O_CREAT|O_TRUNC,0666);
-    read(fd_rqfile,buffer_numpedidos,sizeof(buffer_numpedidos)); //Podiamos optar por uma solução mais bonita a partir do fstat
-
-    int num_pedidos = atoi(buffer_numpedidos); 
 
     pthread_create(&KeyAgent_ID,NULL,KeyAgent,&args_keyagent);
     pthread_create(&CommManager_ID,NULL,CommAgent,&args_commagent);
