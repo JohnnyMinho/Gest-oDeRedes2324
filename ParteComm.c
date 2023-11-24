07b4a5f3dd22189608262ac86f4143eb18e69e5e
JohnnyMinho <jpsousamoura2000@gmail.com>
2023-11-24 11:53:02 +0000

    Push da parte de comunicação sozinha

---
+++ ParteComm.c
@@ -0,0 +1,40 @@
+//No server usamos a solução sugerida pelo professor que é o uso de um ficheiro com o número de pedidos total
+
+void *CommAgent(void *arg){
+
+    int Udp_Server_Socket_fd = (AF_INET, SOCK_DGRAM, 0); //Descriptor da socket UDP, AF_INET especifica que vai trabalhar sobre IpV4
+    dados_CommAgent *dados = (dados_CommAgent *)arg;
+    char server_message[2000], client_message[2000];
+    struct sockaddr_in server; //Esta socket é mais por questões de facilitar a vida ao usar o inet
+    char buffer_numpedidos[11];
+    buffer_numpedidos[11] = '\0';
+
+    int fd_rqfile = open("Num_Requests.txt",O_RDWR|O_CREAT|O_TRUNC,0666);
+    read(fd_rqfile,buffer_numpedidos,sizeof(buffer_numpedidos)); //Podiamos optar por uma solução mais bonita a partir do fstat
+
+    int  num_pedidos = atoi(buffer_numpedidos); 
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = dados->IP_add;
+    server.sin_port = dados->port;
+
+    if(Udp_Server_Socket_fd < 0){
+        printf("UDP socket creation wasn't possible, shutting down!");
+        exit(-1); //Podemos trocar por um exit;
+    } //Erro na Criação
+
+    //Basicamente dizer que a socket passa a ter estas propriedades
+    if(bind(Udp_Server_Socket_fd, (struct sockaddr*)&server, sizeof(server)) < 0){
+        printf("Couldn't bind to the port\n");
+        exit(-2);
+    }
+
+    //Verificamos se a socket ficou atribuída
+    socklen_t len_nome_server = sizeof(server);
+    
+    if(getsockname(Udp_Server_Socket_fd,&server,len_nome_server)){
+        printf("Port failed to be trully attributed to the specified parameters");
+        exit(-3);
+    }
+
+}
